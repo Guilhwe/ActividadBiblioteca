732a1a15b8ed01bf332dce81ffd1a21b3cc49fec
Guilherme Melo <guilherme96melo@gmail.com>
2024-07-25 00:18:09 +0200

    metodo para cambiar la diponibilidad de un libro de de true a false

--- Libro.py
+++ Libro.py
@@ -7,9 +7,15 @@ class Libro:
 
     def __str__(self):
         return f'Libro :{self.titulo} | Autor :{self.autor} | Año: {self.año_de_publicacion}'
+    
+    def alquiler_libro(self):
+        self.disponible = False
 
-libro1=Libro('Harry Porker','Guilherme', 680)
+libro1=Libro('Harry Porker','Guilherme', 680,)
 libro2=Libro('Billy y Mandy', 'Nico', 2000)
 
 print(libro1)
 print(libro2)
+print(libro1.disponible)
+libro1.alquiler_libro()
+print(libro1.disponible)
