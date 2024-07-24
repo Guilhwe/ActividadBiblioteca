88882fe161d3eed0f974464ddbb0bf2fb56e84da
Guilherme Melo <guilherme96melo@gmail.com>
2024-07-25 00:07:30 +0200

    Creacion clase libro y dos ejemplos de prueba

---
+++ Libro.py
@@ -0,0 +1,15 @@
+class Libro:
+    def __init__(self, titulo ='', autor='', año_de_publicacion = 0):
+        self.titulo = titulo
+        self.autor = autor
+        self.año_de_publicacion = año_de_publicacion
+        self.disponible = True
+
+    def __str__(self):
+        return f'Libro :{self.titulo} | Autor :{self.autor} | Año: {self.año_de_publicacion}'
+
+libro1=Libro('Harry Porker','Guilherme', 680)
+libro2=Libro('Billy y Mandy', 'Nico', 2000)
+
+print(libro1)
+print(libro2)
